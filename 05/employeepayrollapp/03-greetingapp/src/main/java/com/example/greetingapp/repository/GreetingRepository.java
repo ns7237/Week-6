8812ec5de575c21941cbaead978c588abb93b6af
ns7237 <101319764+ns7237@users.noreply.github.com>
2025-05-21 11:50:51 +0530

    Delete 05 directory

--- 05/employeepayrollapp/03-greetingapp/src/main/java/com/example/greetingapp/repository/GreetingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.greetingapp.repository;
-
-import com.example.greetingapp.model.GreetingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GreetingRepository extends JpaRepository<GreetingEntity, Long> {
-}
